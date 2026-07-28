1d5959e3083493119066b88748fecb5d5ef508aa
tamura shingo <tamura.shingo@gmail.com>
2026-07-28 23:32:23 +0900

    get memory

--- src/c/main.c
+++ src/c/main.c
@@ -47,6 +47,79 @@ typedef struct {
 } EFI_SYSTEM_TABLE;
 
 
+typedef struct {
+    UINT32 Type;
+    UINT32 Pad;
+    UINT64 PhysicalStart;
+    UINT64 VirtualStart;
+    UINT64 NumberOfPages;
+    UINT64 Attribute;
+} EFI_MEMORY_DESCRIPTOR;
+
+
+#define EfiConventionalMemory 7
+
+typedef EFI_STATUS (EFIAPI *EFI_GET_MEMORY_MAP)(
+    UINTN *MemoryMapSize,
+    EFI_MEMORY_DESCRIPTOR *MemoryMap,
+    UINTN *MapKey,
+    UINTN *DescriptorSize,
+    UINT32 *DescriptorVersion
+);
+
+
+typedef struct _EFI_BOOT_SERVICES {
+    EFI_TABLE_HEADER Hdr;
+
+    void *RaiseTPL;
+    void *RestoreTPL;
+
+    void *AllocatePages;
+    void *FreePages;
+    EFI_GET_MEMORY_MAP GetMemoryMap;
+    void *AllocatePool;
+    void *FreePool;
+
+    void *CreateEvent;
+    void *SetTimer;
+    void *WaitForEvent;
+    void *SignalEvent;
+    void *CloseEvent;
+    void *CheckEvent;
+
+    void *InstallProtocolInterface;
+    void *ReinstallProtocolInterface;
+    void *UninstallProtocolInterface;
+    void *HandleProtocol;
+    void *Reserved;
+    void *RegisterProtocolNotify;
+    void *LocateHandle;
+    void *LocateDevicePath;
+    void *InstalLConfigurationTable;
+
+    void *LoadImage;
+    void *StartImage;
+    void *Exit;
+    void *UnloadImage;
+    void *ExitBootServices;
+
+    void *GetNextMonotonicCount;
+    void *Stall;
+    void *SetWatchdogTimer;
+
+    void *ConnectController;
+    void *DisconnectController;
+
+    void *OpenProtocol;
+    void *CloseProtocol;
+    void *OpenProtocolInformation;
+
+    void *ProtocolsPerHandle;
+    void *LocateHandleBuffer;
+    void *LocateProtocol;
+} EFI_BOOT_SERVICES;
+
+
 typedef EFI_STATUS (EFIAPI *EFI_TEXT_STRING)(struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, CHAR16 *String);
 typedef EFI_STATUS (EFIAPI *EFI_TEXT_CLEAR_SCREEN)(struct _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This);
 
@@ -74,10 +147,91 @@ typedef struct _EFI_SIMPLE_TEXT_INPUT_PROTOCOL {
 } EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
 
 
+char memory_map_buffer[1024 * 256];
+
+
+void UINT64ToHexStr(UINT64 val, CHAR16 *str) {
+    char hex[] = "0123456789ABCDEF";
+    for (int i = 15; i >= 0; i--) {
+        str[i + 2] = hex[val & 0xF];
+        val >>= 4;
+    }
+    str[0] = L'0';
+    str[1] = L'x';
+    str[18] = L'\0';
+}
+
 EFI_STATUS EFIAPI EfiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
     // clear screen
     SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
     SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello from isikiOS\r\n");
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Memory checking...\r\n");
+
+
+    UINTN memory_map_size = 0;
+    EFI_MEMORY_DESCRIPTOR *memory_map = (void *)0;
+    UINTN map_key = 0;
+    UINTN descriptor_size = 0;
+    UINT32 descriptor_version = 0;
+    EFI_STATUS status;
+    UINT64 max_free_size = 0;
+    UINT64 heap_start = 0;
+
+    status = SystemTable->BootServices->GetMemoryMap(
+        &memory_map_size, memory_map, &map_key, &descriptor_size, &descriptor_version
+    );
+
+    memory_map = (EFI_MEMORY_DESCRIPTOR *)memory_map_buffer;
+    memory_map_size = sizeof(memory_map_buffer);
+
+    status = SystemTable->BootServices->GetMemoryMap(
+        &memory_map_size, memory_map, &map_key, &descriptor_size, &descriptor_version
+    );
+
+    if (status == 0) {
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Success to get Memory Map!\r\n");
+
+        UINTN entries = memory_map_size / descriptor_size;
+
+        for (UINTN i = 0; i < entries; i++) {
+            EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)((char *)memory_map + (i * descriptor_size));
+
+            if (desc->Type == EfiConventionalMemory) {
+                UINT64 size = desc->NumberOfPages * 4096;
+                if (size > max_free_size) {
+                    max_free_size = size;
+                    heap_start = desc->PhysicalStart;
+                }
+            }
+        }
+
+        CHAR16 hex_addr[20];
+        CHAR16 hex_size[20];
+        UINT64ToHexStr(heap_start, hex_addr);
+        UINT64ToHexStr(max_free_size, hex_size);
+
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"OS Heap Target Address: ");
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, hex_addr);
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"\r\n");
+
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Available Heap Size: ");
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, hex_size);
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"\r\n");
+    } else {
+        CHAR16 hex_status[20];
+        CHAR16 hex_needed[20];
+        UINT64ToHexStr((UINT64)status, hex_status);
+        UINT64ToHexStr((UINT64)memory_map_size, hex_needed);
+
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Failed to get Memory Map. Status: ");
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, hex_status);
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, L" / Needed SIze: ");
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, hex_needed);
+        SystemTable->ConOut->OutputString(SystemTable->ConOut, L"\r\n");
+        for (;;) {
+        }
+    }
+   
 
     for (;;) {
     }
